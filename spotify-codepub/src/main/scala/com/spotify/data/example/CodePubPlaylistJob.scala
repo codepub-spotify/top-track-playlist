e62153347f5b40a8fae2e2c028386731677938c6
NaHeon Kim <naheonk@spotify.com>
2018-10-03 11:25:59 +0200

    Split pipeline codes

--- spotify-codepub/src/main/scala/com/spotify/data/example/CodePubPlaylistJob.scala
+++ spotify-codepub/src/main/scala/com/spotify/data/example/CodePubPlaylistJob.scala
@@ -3,6 +3,7 @@ package com.spotify.data.example
 import com.spotify.data.api.SpotifyApiCaller
 import com.spotify.data.api.schemas.{CodePubTrack, CodePubTrackAudioFeatures}
 import com.spotify.scio._
+import com.spotify.scio.values.SCollection
 
 import scala.io.Source
 
@@ -19,30 +20,39 @@ object CodePubPlaylistJob {
     // Get the top tracks for your user
     val topTracksRequest: Seq[CodePubTrack] = SpotifyApiCaller.getTopTracks()
 
-    // Step1. Extract the IDs from your top tracks
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Step1. Extract the Ids from your top tracks
     // First check how `CodePubTrack.avsc` looks like and remember Avro magic from slides!
     // Here's one more hint: use `.toString` to convert to String.
     val ids: Seq[String] = topTracksRequest.map(???)
+    ///////////////////////////////////////////////////////////////////////////////////////////////
 
     // Get audio features for your top tracks
     val topTrackAudioFeatures: Seq[CodePubTrackAudioFeatures] = SpotifyApiCaller.getTrackAudioFeatures(ids)
 
-    sc
-      // Convert your top track audio features to an SCollection
-      .parallelize(topTrackAudioFeatures)
-      // Step2. Use a filter to select your mood of the playlist.
-      // Check out `CodePubTrackAudioFeatures.avsc` schema and explore which features are available.
-      // You already have some recipes inside slides.
-      // For example, I'd like to create a playlist for dance with danceability > 0.7 songs
-      // BE CREATIVE!!
-      .filter(???)
-      // Step3. Extract the track IDs
-      // You need schema here again. We did the similar thing already, right?
-      .map(???)
-      // Save the results to a text file
-      // DISCLAIMER: We need to save it as a text file in order for the playlist API call to
-      // be able to read the output. Normally you would not do this part in a pipeline.
-      .saveAsTextFile("playlist-tracks", numShards = 1)
+    // Convert your top track audio features to an SCollection
+    // Don't be afraid of SCollection. You could think it as Seq or Array.
+    val parallelTrackAudioFeatures: SCollection[CodePubTrackAudioFeatures] = sc.parallelize(topTrackAudioFeatures)
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Step2. Use a filter to select your mood of the playlist.
+    // Check out `CodePubTrackAudioFeatures.avsc` schema and explore which features are available.
+    // You already have some recipes inside slides.
+    // For example, I'd like to create a playlist for dance with danceability > 0.7 songs
+    // BE CREATIVE!!
+    val trackAudioFeaturesInMood = parallelTrackAudioFeatures.filter(???)
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // Step3. Extract the track Ids
+    // You need schema here again. We did the similar thing already, right?
+    val trackIdsInMood = trackAudioFeaturesInMood.map(???)
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Save the results to a text file
+    // DISCLAIMER: We need to save it as a text file in order for the playlist API call to
+    // be able to read the output. Normally you would not do this part in a pipeline.
+    trackIdsInMood.saveAsTextFile("playlist-tracks", numShards = 1)
 
     // Kick off the pipeline and wait for the results
     sc.close().waitUntilDone()
@@ -56,8 +66,10 @@ object CodePubPlaylistJob {
     // Fetch your user ID based on the token you provided
     val userId = SpotifyApiCaller.getUserID()
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////
     // Step4. Define your playlist name. For example, "CodePub Playlist!"
     val playlistName = ???
+    ///////////////////////////////////////////////////////////////////////////////////////////////
 
     // Create a new playlist for your user
     SpotifyApiCaller.createPlaylist(userId, playlistName)
